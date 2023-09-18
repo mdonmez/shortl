b41442169af7e9d6a09d51a962ecd868d5131844
Muhammed <124686256+mdonmez@users.noreply.github.com>
2023-09-18 22:02:24 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='shortl',
+    version='0.0.1',
+    description='A Python module for simple link shortening.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mdonmez/shortl',
+    author='mdonmez',
+    license='GPL-3.0-or-later',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=['requests'],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+    ],
+)
