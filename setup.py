60d2ce33cd86b180f6f8999507ed7aa1079108ec
Muhammed <124686256+mdonmez@users.noreply.github.com>
2023-09-18 22:36:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ setup(
     url='https://github.com/mdonmez/shortl',
     author='mdonmez',
     license='GPL-3.0-or-later',
-    package_dir={"": "src"},
+    package_dir={"shortl.py": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=['requests'],
+    install_requires=['requests', 'clipboard'],
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
